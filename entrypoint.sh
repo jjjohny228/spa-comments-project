1c2d3cf47d95dcd332f76b0cac9c1b7006a816e6
jjjohny228 <gggleb13@gmail.com>
2024-06-15 12:05:37 +0300

    Created project spa and app comments. Added workflow file django.yaml. Added .dockerignore, requirements.txt, entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# prepare backend stuff, think of running collect static and compilemessages in build step
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+# run web server
+gunicorn config.wsgi --bind 0.0.0.0:8000 --access-logfile -
